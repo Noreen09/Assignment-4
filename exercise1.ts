a7f66c6acca0f73aa3f3f86a10cf3de44ee27642
Noreen <noumanabid012@gmail.com>
2023-12-03 09:44:23 +0500

    first commit

---
+++ exercise1.ts
@@ -0,0 +1,7 @@
+// Exercise 1:  Write a program that calculates the area of a rectangle.
+
+function rectangle_area(width: number, height: number) {
+  console.log(height * width);
+}
+
+rectangle_area(2, 2);

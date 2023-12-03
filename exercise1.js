39a3923f07be48ae1c33cf5472e6e49e49774e94
Noreen <noumanabid012@gmail.com>
2023-12-03 09:44:52 +0500

    second commit

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exercise 1:  Write a program that calculates the area of a rectangle.
+function rectangle_area(width, height) {
+    console.log(height * width);
+}
+rectangle_area(2, 2);

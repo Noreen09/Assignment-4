8f5869f3a4d6f876f3d41a26a95d555cf98ea49c
Noreen <noumanabid012@gmail.com>
2023-12-03 09:46:40 +0500

    first commit

---
+++ exercise3.js
@@ -0,0 +1,13 @@
+"use strict";
+// Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 0;
+if (a < 0) {
+    console.log("a is negative");
+}
+else if (a > 0) {
+    console.log("a is positive");
+}
+else if (a == 0) {
+    console.log("a is zero");
+}
---
+++ exercise3.ts
@@ -0,0 +1,13 @@
+// Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+
+let a: number = 0;
+
+if (a < 0) {
+  console.log("a is negative");
+} else if (a > 0) {
+  console.log("a is positive");
+} else if (a == 0) {
+  console.log("a is zero");
+}
+
+export {};

98f393b2020a0cb2e35399299f83054147775242
Noreen <noumanabid012@gmail.com>
2023-12-03 09:46:13 +0500

    first commit

---
+++ exercise2.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exercise 2: Write a program that takes input and calculates the volume of a cube.
+function cube_volume(input) {
+    console.log(input ** 3);
+}
+cube_volume(2);
---
+++ exercise2.ts
@@ -0,0 +1,7 @@
+// Exercise 2: Write a program that takes input and calculates the volume of a cube.
+
+function cube_volume(input: number) {
+  console.log(input ** 3);
+}
+
+cube_volume(2);
